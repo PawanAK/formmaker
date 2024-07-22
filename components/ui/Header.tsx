dd295639eaeaf70a2e5b0fabb3d18dd9a4fb01f0
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-07-22 15:10:31 +0530

    add Header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,7 +4,6 @@ import { auth, signIn, signOut } from "../../app/auth";
 import Link from "next/link";
 import { Button } from "./button";
 import Image from "next/image";
-
 type Props = {};
 
 function SignOut() {
@@ -21,19 +20,32 @@ function SignOut() {
 
 const Header = async (props: Props) => {
   const session = await auth();
-  console.log(session);
 
   return (
-    <header>
-      <nav>
-        <div>
-          <h1>Formify - AI form Builder</h1>
+    <header className="border bottom-1">
+      <nav className="bg-white border-gray-200 px-4 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <h1>AI Form Builder</h1>
           <div>
             {session?.user ? (
-              <div>session.user.name</div>
+              <div className="flex items-center gap-4">
+                <Link href="/view-forms">
+                  <Button variant="outline">Dashboard</Button>
+                </Link>
+                {session.user.name && session.user.image && (
+                  <Image
+                    src={session.user.image}
+                    alt={session.user.name}
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                  />
+                )}
+                <SignOut />
+              </div>
             ) : (
               <Link href="/api/auth/signin">
-                <Button>Sign in</Button>
+                <Button variant="link">Sign in</Button>
               </Link>
             )}
           </div>
