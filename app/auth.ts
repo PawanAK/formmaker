8ada7f9b039470fbf285e2fe034a8936e164d9da
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-08-01 15:23:04 +0530

    add migration

--- app/auth.ts
+++ app/auth.ts
@@ -1,12 +1,15 @@
+import { db } from "./../db/index";
 import NextAuth from "next-auth";
-
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GoogleProvider from "next-auth/providers/google";
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
